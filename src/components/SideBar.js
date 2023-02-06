502156866de02e6d5578be63eae5ab2c248b5d6e
Eric Jones <Joneseric978@aol.com>
2023-02-06 18:48:50 -0500

    edits

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,11 +2,12 @@ const SideBar = () => {
   return (
     <div>
       <div>
-        <h1>Game Day</h1>
+        <h1>GAME DAY</h1>
         <h2>
           Feb 19 Sun • [7:00pm EST] Game Vivint Smart Home Arena - Salt Lake
           City, UT . . . . . . . . . . . . . 12 players each team
         </h2>
+        <p> Lebron= 3-0 Giannias 0-3</p>
       </div>
     </div>
   )
