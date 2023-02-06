b5c4e2deb1c6d0bf95aaf6ec7da4e4eff3b914d2
Eric Jones <Joneseric978@aol.com>
2023-02-06 18:58:58 -0500

    part 14

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,7 +7,7 @@ const SideBar = () => {
           Feb 19 Sun • [7:00pm EST] Game Vivint Smart Home Arena - Salt Lake
           City, UT . . . . . . . . . . . . . 12 players each team
         </h2>
-        <p> Lebron= 3-0 Giannias 0-3</p>
+        <p> Lebron= 3-0 . . .Giannias 0-3</p>
       </div>
     </div>
   )
